f813b20199db0e7160d3fa397b3bce18e1333248
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-06-30 14:30:38 +0800

    change section-2 a little

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,29 +1,3 @@
-/*function count_same_elements(collection) {
-    var result=[];
-  
-    for(var i=0;i<collection.length;i++){
-      var counts=1;
-      var attr='';
-      while(collection.length>1?(collection[i][0]==attr||i==0):(collection[i]==attr||i==0)){
-        if(collection[i].length==1){
-          counts++;
-          attr=collection[i];
-        }else if(collection.length==5){
-          counts+=((collection[i][2]-0)*10+(collection[i][3]-0));
-          attr=collection[i][0];
-        }else{
-          counts+=(collection[i][2]-0);
-          attr=collection[i][0];
-        }
-        attr=collection[i]
-        i++;
-      }
-      result.push({key:collection[i],summary:counts});
-    }
-    return result;
-}
-*/
-
 function count_same_elements(collection) {
     var result=[];
    // var i=0;
@@ -33,6 +7,7 @@ function count_same_elements(collection) {
       //alert(i);
       attr=collection[i].length>1?collection[i][0]:collection[i];
       //attr=collection[i][0];
+      var l=collection[i].length;
       while(collection[i].length>1?collection[i][0]==attr:collection[i]==attr) {
         if(collection[i].length==1){
           counts++;
@@ -52,7 +27,9 @@ function count_same_elements(collection) {
        // } else if (collection[i].length>1&&collection[i][0]!=collection[i+1]) {
        //  break;
        // }
+
         i++;
+        l=collection[i].length;
       }
       result.push({key:attr,summary:counts});
       //i--;
